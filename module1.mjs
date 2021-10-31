e61bf09f50f0c9b51116760b88c5f71f4382ba51
Amulya Ojha <amulyaojha2@gmail.com>
2021-11-01 01:37:42 +0530

    removing the modules

--- module1.mjs
+++
@@ -1,6 +0,0 @@
-const c="Amulya";
-const a=1;
-const d=2;
-export default c;
-export {a};
-export {d};
--- module2.mjs
+++
@@ -1,4 +0,0 @@
-import k, {a,d} from './module1.mjs'
-console.log(k);
-console.log(a);
-console.log(d);
