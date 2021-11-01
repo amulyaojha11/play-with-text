0ffc02096d7c9d92af63d382811814c8f29c61ef
Amulya Ojha <amulyaojha2@gmail.com>
2021-11-01 16:04:41 +0530

    enabling dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
       <Navbar title="PlayWithText" about="About"/>
-      <div className="container my-3">
+      {/* <div className="container my-3">
         <TextForm heading="Enter the text to analyze below" />
-      </div>
+      </div> */}
+      <About/>
     </>
   );
 }
