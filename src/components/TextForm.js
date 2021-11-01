c3758d235f3d9d237c945cebabe47391b79139a5
Amulya Ojha <amulyaojha2@gmail.com>
2021-11-02 01:53:00 +0530

    dark mode in buttons

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -39,11 +39,11 @@ export default function TextForm(props) {
             <div className="mb-3"> 
             <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
-            <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
-            <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
-            <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+            <button type="button" className={`btn btn-${props.mode==='light'?'dark':'light'} mx-1`} onClick={handleUpClick}>Convert to Uppercase</button>
+            <button type="button" className={`btn btn-${props.mode==='light'?'dark':'light'} mx-1`} onClick={handleLowClick}>Convert to Lowercase</button>
+            <button type="button" className={`btn btn-${props.mode==='light'?'dark':'light'} mx-1`} onClick={handleClearClick}>Clear Text</button>
+            <button type="button" className={`btn btn-${props.mode==='light'?'dark':'light'} mx-1`} onClick={handleCopy}>Copy Text</button>
+            <button type="button" className={`btn btn-${props.mode==='light'?'dark':'light'} mx-1`} onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h2>Your text summary</h2>
